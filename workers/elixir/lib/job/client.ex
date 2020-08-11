e33facf4c1f4cdc67f06c25fb9e42ae96744f9cf
Praveen Perera <ppraveen25@gmail.com>
2020-08-10 21:18:05 -0400

    Improve error handling on client peform

--- workers/elixir/lib/job/client.ex
+++ workers/elixir/lib/job/client.ex
@@ -4,20 +4,18 @@ defmodule Bossman.Job.Client do
 
   @spec perform(%{value: String.t()}, %{value: String.t()}, Options.t()) :: {:ok, any}
   def perform(name, docker_image_name, options) do
-    {:ok, channel} = GRPC.Stub.connect("localhost:50051")
-
-    request =
-      Bossman.Protobuf.V1alpha1.Job.PerformRequest.new(
-        name: name,
-        docker_image_name: docker_image_name,
-        options: options
-      )
-
-    {:ok, reply} = Bossman.Protobuf.V1alpha1.JobService.Stub.perform(channel, request)
-
-    Task.start(fn -> GRPC.Stub.disconnect(channel) end)
-
-    {:ok, reply}
+    with {:connect, {:ok, channel}} <- {:connect, GRPC.Stub.connect("localhost:50051")},
+         request <-
+           Bossman.Protobuf.V1alpha1.Job.PerformRequest.new(
+             name: name,
+             docker_image_name: docker_image_name,
+             options: options
+           ),
+         {:reply, {:ok, reply}} <-
+           {:reply, Bossman.Protobuf.V1alpha1.JobService.Stub.perform(channel, request)} do
+      Task.start(fn -> GRPC.Stub.disconnect(channel) end)
+      {:ok, reply}
+    end
   end
 
   def get() do
