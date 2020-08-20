084e8da4ba1e75552bfbaa7e7756217fc854f88f
Praveen Perera <ppraveen25@gmail.com>
2020-08-20 16:31:46 -0400

    Decode status correctly in elixir worker

--- workers/elixir/lib/job.ex
+++ workers/elixir/lib/job.ex
@@ -1,5 +1,5 @@
 defmodule Bossman.Job do
-  alias Bossman.Job.{Client, Options, Decode}
+  alias Bossman.Job.{Client, Options, Decode, Status}
   alias Bossman.Util.Result
   use TypedStruct
 
@@ -10,10 +10,6 @@ defmodule Bossman.Job do
     field :docker_image_name, String.t(), required: true
     field :options, Options.t(), required: true
     field :status, Job.Status.t()
-  end
-
-  defmodule Status do
-    @type t :: Bossman.Protobuf.V1alpha1.Job.Status.t()
   end
 
   @spec perform(String.t(), String.t(), map()) :: {:ok, any} | {:error, any}
@@ -51,8 +47,9 @@ defmodule Bossman.Job do
 
   @spec get_status(String.t()) :: {:ok, Job.Status.t()} | {:error, any}
   def get_status(id) do
-    with {:ok, reply} <- Client.get_status(id) do
-      {:ok, reply.status}
+    with {:ok, reply} <- Client.get_status(id),
+         {:ok, status} <- Status.decode(reply.status) do
+      {:ok, status}
     else
       error -> error
     end
---
+++ workers/elixir/lib/job/status.ex
@@ -0,0 +1,50 @@
+defmodule Bossman.Job.Status do
+  use TypedStruct
+
+  defmodule Error do
+    defexception [:message]
+  end
+
+  @type t :: :waiting | Active.t() | Completed.t() | Failed.t()
+
+  defmodule Active do
+    typedstruct do
+      field :started_at, String.t()
+    end
+  end
+
+  defmodule Completed do
+    typedstruct do
+      field :started_at, String.t()
+      field :completed_at, String.t()
+    end
+  end
+
+  defmodule Failed do
+    typedstruct do
+      field :started_at, String.t()
+      field :failed_at, String.t()
+    end
+  end
+
+  @spec decode(Bossman.Protobuf.V1alpha1.Job.Status.t()) ::
+          {:ok, Status.t()} | {:error, String.t()}
+  def decode(status) do
+    try do
+      {:ok, decode!(status)}
+    catch
+      error ->
+        {:error, %Error{message: "Unable to decode status: #{inspect(error)}"}}
+    end
+  end
+
+  @spec decode!(Bossman.Protobuf.V1alpha1.Job.Status.t()) :: Status.t()
+  def decode!(%_{status: {:waiting, _waiting}}), do: :waiting
+  def decode!(%_{status: {:active, active}}), do: %Active{started_at: active.started_at}
+
+  def decode!(%_{status: {:completed, completed}}),
+    do: %Completed{started_at: completed.started_at, completed_at: completed.completed_at}
+
+  def decode!(%_{status: {:failed, failed}}),
+    do: %Failed{started_at: failed.started_at, failed_at: failed.failed_at}
+end
